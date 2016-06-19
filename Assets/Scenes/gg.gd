3d724b331d8a1d93d0de3ca42e71feb0674b7f69
George Linkovsky <krougz@live.ru>
2016-06-19 04:10:35 +0400

    Small changes #2 Added: * Files sounds: ** Sound of hitting a tree ** Sound of hitting the stone ** The sound of a falling tree ** The sound of footsteps * The sound of footsteps * Stone extraction (sound + debug)
    Modified:
    * Removed the script "testmap.gd"

--- Assets/Scenes/gg.gd
+++ Assets/Scenes/gg.gd
@@ -3,6 +3,11 @@ extends KinematicBody2D
 
 var MOTION_SPEED = 100 # Pixels/second
 var playing_anim = "idle"
+var right_hand = true
+var weapon_type = 0
+# 0 - NO WEAPON
+
+var timer_stop = true
 
 func _fixed_process(delta):
 	var motion = Vector2()
@@ -18,11 +23,17 @@ func _fixed_process(delta):
 	
 	motion = motion.normalized()*MOTION_SPEED*delta
 	if motion == Vector2(0,0):
+		if (not timer_stop):
+			get_node("Timer").stop()
+			timer_stop = true
 		get_node("Legs").set_rot(get_node("Body").get_rot())
 		if playing_anim != "idle":
 			get_node("leg_anim").play("idle")
 			playing_anim = "idle"
 	else:
+		if (timer_stop):
+			get_node("Timer").start()
+			timer_stop = false
 		if playing_anim != "walk":
 			get_node("leg_anim").play("walk")
 			playing_anim = "walk"
@@ -42,7 +53,27 @@ func _fixed_process(delta):
 	else:
 		get_node("Legs").show()
 		MOTION_SPEED = 100
+	
+	if (Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		if (weapon_type == 0):
+			if (not get_node("body_anim").is_playing()):
+				if (right_hand):
+					get_node("body_anim").play("blow_right")
+					right_hand = false
+				else:
+					get_node("body_anim").play("blow_left")
+					right_hand = true
+				if (get_node("Body/ray").is_colliding()):
+					print (get_node("Body/ray").get_collider().get_name())
+					if (get_node("Body/ray").get_collider().get_name() == "rocks"):
+						print ("extracted stone")
+						get_node("player").play("rock_hand_blow")
 
 func _ready():
 	set_fixed_process(true)
 
+func _on_Timer_timeout():
+	if (get_parent().get_node("map").get_cellv(get_parent().get_node("map").world_to_map( get_pos())) == 1):
+		pass
+	else:
+		get_node("player").play("run_ground")
