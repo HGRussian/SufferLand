1a27f0ea24a12f7c49a03f0d0c9aacb56d5356f9
Dm.Derbin <dds9435@gmail.com>
2016-06-30 19:18:36 +0300

    debug panel

--- Assets/Scripts/splash.gd
+++ Assets/Scripts/splash.gd
@@ -4,4 +4,12 @@ extends Node2D
 func _ready():
 	pass
 
+func _on_bt_stopZombie_pressed():
+	var zombies = get_parent().get_node('Zombie')
+	for zombie in zombies.get_children():
+		zombie.activ=false
 
+func _on_bt_runZombie_pressed():
+	var zombies = get_parent().get_node('Zombie')
+	for zombie in zombies.get_children():
+		zombie.activ=true
