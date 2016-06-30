3a0ed21b9620b04eb100e6e12a22dfd18f840ad6
Dm.Derbin <dds9435@gmail.com>
2016-06-30 15:38:52 +0300

    ref sound

--- Assets/Scripts/game.gd
+++ Assets/Scripts/game.gd
@@ -1,19 +1,26 @@
 
 extends Node2D
-onready var loadScene = load('Assets/Scenes/load.scn').instance()
+onready var splash = load('Assets/Scenes/load.scn').instance()
 onready var worldGen = get_node("worldGen/grnd") 
 onready var gg = get_node("player")
 
 #UI
-onready var progressBar = loadScene.get_node("cl/tpb_load")
-onready var lvlSplash = loadScene.get_node("cl/tf_nameLevel")
-onready var sp_noise = loadScene.get_node("sp_noise")
+onready var progressBar = splash.get_node("cl/tpb_load")
+onready var lvlSplash = splash.get_node("cl/tf_nameLevel")
+onready var sp_noise = splash.get_node("sp_noise")
+onready var sp_gameSound = get_node("sp_gameSound")
 
 func hide_splash():
 	progressBar.set_hidden(true)
 	lvlSplash.set_hidden(true)
-	sp_noise.set_stream(load("res://Assets/Sounds/HorrorMusic.ogg"))
+	sp_noise.stop()
+	sp_gameSound.play()
+	
+func show_splash():
+	progressBar.set_hidden(false)
+	lvlSplash.set_hidden(false)
 	sp_noise.play()
+	sp_gameSound.stop()
 	
 var level = 0
 var levelSize = [2048]
@@ -25,7 +32,7 @@ var genStarted = false
 var genEnded = false
 
 func _ready():
-	get_node(".").add_child(loadScene)
+	get_node(".").add_child(splash)
 	set_process(true)
 
 func _process(delta):
